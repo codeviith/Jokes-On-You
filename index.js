1ebd53e911564a5ee6104045df9f5128ad9af21b
codeviith <127069727+codeviith@users.noreply.github.com>
2023-08-15 11:11:24 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,28 @@
-
-// let setupHistory;
-// let punchlineHistory;
-
 const jokeDisplay = document.querySelector(".container-2");
 const generateJokeButton = document.getElementById("generate-joke-button");
 const showPunchlineButton = document.getElementById("show-punchline");
 
-const displayedJoke = document.createElement("li");
-const displayedPunchline = document.createElement("li");
+const displayedJoke = document.getElementById("setup");
+const displayedPunchline = document.getElementById("punchline");
 
 const jokesBox = document.querySelector(".container-1");
+const jokesBoxForm = document.getElementById("jokes-box-form");
 
-// const currentPunchline = document.getElementById("joke-punchline");
-// const jokeButtonDiv = document.getElementById("joke-button");
+const jokesHistory = document.getElementById("joke-container");
 
+let currentJoke;
 
 fetch("http://localhost:3500/Jokes")
 .then(response => response.json())
 .then(jokes => {
     generateJokeButton.addEventListener("click", () => {
-        const randomNum = randomId();
+        let randomNum = randomId();
 
         jokes.find(joke => {
             if (randomNum === joke.id) {
+                // jokesHistory.textContent = "";
+                currentJoke = joke;
                 displayJokeSetup(joke);
-
-                showPunchlineButton.addEventListener("click", () => {
-                    displayPunchline(joke);
-
-                    displayedPunchline.textContent = jokeDisplay.textContent;
-                    jokesBox.appendChild(displayedPunchline);
-                    // punchlineHistory = displayedPunchline;
-                });
             }
 
             displayedJoke.textContent = jokeDisplay.textContent;
@@ -39,13 +30,26 @@ fetch("http://localhost:3500/Jokes")
             // setupHistory = displayedJoke;
         });
     });
+
+    showPunchlineButton.addEventListener("dblclick", () => {
+        displayPunchline(currentJoke);
+
+        const jokeHistoryTag = document.createElement("li");
+        jokeHistoryTag.textContent = displayedJoke.textContent;
+
+        jokesHistory.appendChild(jokeHistoryTag);
+    });
 });
 
 
+// jokesBoxForm.addEventListener("submit", (e) => {
+//     e.preventDefault();
+
+
+// })
 
 function randomId() {
     let random = Math.ceil(Math.random() * 20);
-    // console.log(random);
     return random;
 }
 
@@ -57,17 +61,3 @@ function displayPunchline(joke) {
     jokeDisplay.textContent = joke.punchline;
 }
 
-
-
-
-
-
-// let likeCount;
-
-// const likeButtonTag = document.createElement("button");
-// likeButtonTag.textContent = "Like"
-
-// document.body.appendChild(likeButtonTag);
-
-// const totalNumOfLikes = document.createElement("span");
-// totalNumOfLikes.textContent = likeCount + "Likes"

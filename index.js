fbda408ad64be21bbb0f705098d34d782f96f91d
codeviith <127069727+codeviith@users.noreply.github.com>
2023-08-15 19:40:08 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,49 +1,44 @@
 const jokeDisplay = document.querySelector(".container-2");
 const generateJokeButton = document.getElementById("generate-joke-button");
 const showPunchlineButton = document.getElementById("show-punchline");
-
 const displayedJoke = document.getElementById("setup");
-const displayedPunchline = document.getElementById("punchline");
-
-const jokesBox = document.querySelector(".container-1");
-const jokesBoxForm = document.getElementById("jokes-box-form");
-
 const jokesHistory = document.getElementById("joke-container");
 
 let currentJoke;
-
+let randomNum;
 
 fetch("http://localhost:3500/Jokes")
 .then(response => response.json())
 .then(jokes => {
     generateJokeButton.addEventListener("click", () => {
-        let randomNum = randomId();
-
+        randomNum = randomId();
+        
         jokes.find(joke => {
             if (randomNum === joke.id) {
-                // jokesHistory.textContent = "";
                 currentJoke = joke;
                 displayJokeSetup(joke);
             }
-
+            
             displayedJoke.textContent = jokeDisplay.textContent;
-            jokesBox.appendChild(displayedJoke);
-            // setupHistory = displayedJoke;
         });
     });
-    
 
     showPunchlineButton.addEventListener("dblclick", () => {
-        displayPunchline(currentJoke);
+        displayPunchline (currentJoke);
+
         const jokeHistoryTag = document.createElement("li");
+        jokeHistoryTag.id = currentJoke.id;
 
         jokeHistoryTag.textContent = displayedJoke.textContent;
-
         jokesHistory.appendChild(jokeHistoryTag);
 
-
         jokeHistoryTag.addEventListener("mouseover", () => {
-            jokeDisplay.textContent = jokeHistoryTag.textContent
+            let tagId = jokeHistoryTag.id;
+            const foundJoke = jokes.find(joke => {
+                return parseInt(tagId) === joke.id;
+            })
+
+            jokeDisplay.textContent = foundJoke.punchline;
         })
     });
 });
@@ -54,11 +49,13 @@ function randomId() {
     return random;
 }
 
+
 function displayJokeSetup (joke) {
-    jokeDisplay.textContent = joke.setup;
+    return jokeDisplay.textContent = joke.setup;
 }
 
+
 function displayPunchline(joke) {
-    jokeDisplay.textContent = joke.punchline;
+    return jokeDisplay.textContent = joke.punchline;
 }
 
